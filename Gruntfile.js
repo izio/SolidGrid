5276d3163970561295d257fd3336b9cfc7b31fcc
Patrick Robin <p.j.robin@soton.ac.uk>
2015-11-16 15:29:17 +0000

    Added missing watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,10 +21,15 @@
                     "combined.min.css": ["base.css", "grid.css"]
                 }
             }
+        },
+        watch: {
+            files: ["*.less"],
+            tasks: ['less']
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask("default", ["less", "cssmin"]);
 };
